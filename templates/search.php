fd09cda7819c6c0f81fb8264ff5819743352dc57
Mahesh Upadhyaya <mahesh@upavadi.net>
2016-04-28 13:36:44 +0100

    Modified for Responsive display

--- templates/search.php
+++ templates/search.php
@@ -1,13 +1,13 @@
-		<?php
-		/*
-		Search TNG database for names
-		*/
-			$tngcontent = Upavadi_TngContent::instance()->init();
-			$user = $tngcontent->getTngUser();
-			$usertree = $user['gedcom'];
-			$tree = $result['gedcom'];
-	
-		?>
+	<?php
+	/*
+	Search TNG database for names
+	*/
+		$tngcontent = Upavadi_TngContent::instance()->init();
+		$user = $tngcontent->getTngUser();
+		$usertree = $user['gedcom'];
+		$tree = $result['gedcom'];
+
+	?>
 
 <form style="display: inline-block;" method="get">
 	<label for="search-lastname">Last Name: <input type="text" value="<?php echo $lastName; ?>" name="lastName" id="search-lastname"></label> 
@@ -16,23 +16,23 @@
 </form>
 <p><h2><span style="color:#D77600; font-size:14pt">Search Results</span></h2></p>
 <?php 
-
 if (!count($results)): ?>
 <h2>No results found, please search again</h2>
 <?php else: ?>
-<table class="form-table" border="1">
-<thead>
+<div class="container col-md-6 col-sm-4 table-responsive">
+<table class="table table-bordered">
+
 	<tr>
-			<th class="theader">Name</th>
-			<th class="theader">Birth Date</th>
+			<td class="tdback col-md-4 col-sm-2">Name</th>
+			<td class="tdback col-md-1 col-sm-1">Birth Date</th>
 			<?php 
 			if ($usertree == '') { ?>
-			<th class="theader">Tree</th>
+			<td class="tdback col-md-1 col-sm-1">Tree</th>
 			
 			<?php } ?>
 			
 	</tr>
-</thead>
+
 <tbody>
 	<?php foreach($results as $result):
 	if ($result['birthdate'] == ''){
@@ -49,16 +49,16 @@ if (!count($results)): ?>
 	//}
 	?>
 	<tr>
-		<td class="tdfront">
+		<td class="col-md-4 col-sm-2">
 			<a href="/family/?personId=<?php echo $result['personID']?>&amp;tree=<?php echo $tree; ?> "><?php echo $firstname . ' ' . $lastname; ?></a>
 		</td>
-		<td  class="tdfront">
+		<td  class="col-md-1 col-sm-1">
 			<?php echo $result['birthdate']; ?>
 		</td>
 		
 	<?php 
 		if ($usertree == '') { ?>
-			<td class="tdfront"><?php echo $result['gedcom']; ?></td>
+			<td class="col-md-1 col-sm-1"><?php echo $result['gedcom']; ?></td>
         </tr>
     <?php
 	}
@@ -66,4 +66,5 @@ if (!count($results)): ?>
 	endforeach; ?> 
 </tbody>
 </table>
+</div>
 <?php endif; ?>
