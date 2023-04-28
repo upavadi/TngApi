8a899cede18137127ee1e01b80ee3d599cf81453
upavadi <mahesh@upavadi.net>
2023-04-28 12:18:57 +0100

    jquery update

--- templates/family.html.php
+++ templates/family.html.php
@@ -852,7 +852,9 @@ $User = $current_user->user_firstname;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Upload photos</title>
     
-	<script type="text/javascript" src="<?php echo plugins_url('js/jquery-1.10.2.min.js', dirname(__FILE__)); ?>"></script>
+    <title>Upload photos</title>
+    	<!-- jquery-current.min.js is copy of current jquery jquery-x.x.x.min.js --> 
+  	<script type="text/javascript" src="<?php echo plugins_url('js/jquery-current.min.js', dirname(__FILE__)); ?>"></script>
 	<script type="text/javascript" src="<?php echo plugins_url('js/jquery.form.min.js', dirname(__FILE__)); ?>"></script>
 
 	<?php 	
